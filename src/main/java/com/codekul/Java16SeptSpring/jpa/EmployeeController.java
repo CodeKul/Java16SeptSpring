874561eb0927d60ff5bd6b15bc1e3cfd365a2538
codekul-vaibhav <codekul.vaibhav@gmail.com>
2021-12-20 17:55:39 +0530

    GetMapping

--- src/main/java/com/codekul/Java16SeptSpring/jpa/EmployeeController.java
+++ src/main/java/com/codekul/Java16SeptSpring/jpa/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.codekul.Java16SeptSpring.jpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -17,4 +20,10 @@ public class EmployeeController {
         employeeRepository.save(employee);
         return "Employee saved..";
     }
+
+    @GetMapping("getEmployee")
+    public List<Employee> getEmployee(){
+        return employeeRepository.findAll();
+    }
+
 }
